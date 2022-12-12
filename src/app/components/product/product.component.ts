863fd4053d3f3c7be40aa0119025a7037e2db12f
Aziz-Ourabiah-Hotmail <ourabiahaziz1@gmail.com>
2022-12-12 15:44:25 -0500

    Création de la logique de la page product

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ProductService } from '../../services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  //@Input() productss: Product | undefined;
 
-  constructor() { }
+  slug: string | undefined;
+  product: Product | undefined;
+  productSub: Subscription | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
+    this.slug = this.route.snapshot.params['slug'];
+    this.productSub = this.productService.getProduct().subscribe({
+      next: (products: Product[]) => {
+        this.product = products.filter((p) => p.slug === this.slug)[0];
+      },
+      error: (error: any) => {
+        console.log("Voilà l'erreur ", error);
+      },
+    });
+
+    //console.log(this.slug);
   }
 
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.productSub?.unsubscribe;
+  }
 }
